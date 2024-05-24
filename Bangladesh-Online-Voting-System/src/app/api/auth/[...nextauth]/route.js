9cfb90ba0ccb7091acead72aa1d3add60afe8ea8
tanvirgb <tspramanik123@gmail.com>
2024-05-24 21:01:54 +0600

    Updated .eslintrc.json file

--- Bangladesh-Online-Voting-System/src/app/api/auth/[...nextauth]/route.js
+++ Bangladesh-Online-Voting-System/src/app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,6 @@ import GithubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
   providers: [
-    // OAuth authentication providers...
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
